d767deb019a7c0f08beb738aff366d30a8ea18ac
rootnure <nur.diu.2791@gmail.com>
2023-10-17 03:43:27 +0600

    post request handler added

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const coffeeCollection = client.db("coffeeDB").collection('coffee');
+
+        app.post('/coffee', async (req, res) => {
+            const newCoffee = req.body;
+            console.log(newCoffee);
+
+            const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        })
 
 
 
