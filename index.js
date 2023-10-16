b3b368e5aed4ab1f45fbe4140c641d2618ec0918
rootnure <nur.diu.2791@gmail.com>
2023-10-17 04:52:33 +0600

    get single data with all field request handler added

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
+        })
+
         app.delete('/coffee/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
